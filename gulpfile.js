94ab9b7a85f7d9c537101aa5482c618b8c99a953
Jan Rehwaldt <jrehwaldt@cedavis.com>
2015-08-28 11:10:12 +0200

    rewrite as less version which does not require a sass install

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,30 @@
-var 
+var
     gulp        = require('gulp'), 
-    sass        = require('gulp-ruby-sass') ,
+    less        = require('gulp-less'),
     prefix      = require('gulp-autoprefixer'),
     minifyCss   = require('gulp-minify-css'),
     rename      = require('gulp-rename'),
     sourcemaps  = require('gulp-sourcemaps');
 
 var config = {
-	scssDir: './scss/',
+	lessDir: './less/',
 	bowerDir: './bower_components/',
 	cssDir: './css/'
 }
-gulp.task('scss',function(){
-    return sass(
-        config.scssDir + '/style.scss',
-        {
-            style: 'expanded',
-             loadPath: [
-                  config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
-                 config.bowerDir + '/fontawesome/scss',
-                 config.scssDir
-            ], 
-              sourcemap: true,
-               trace: true
-         }) 
-        //  .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"))
-        .pipe(sourcemaps.write())
-         .pipe(gulp.dest(config.cssDir))
-        .pipe(rename({suffix: '.min' }))
-        .pipe(minifyCss())
-         .pipe(gulp.dest(config.cssDir))
+gulp.task('less',function(){
+  var paths = [
+  config.bowerDir + '/bootstrap/less',
+ config.lessDir
+  ];
+  return gulp.src( config.lessDir + '**/*.less')
+    .pipe(sourcemaps.init())
+    .pipe(less({paths: paths}))
+    // .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"))
+    .pipe(sourcemaps.write())
+     .pipe(gulp.dest(config.cssDir))
+    .pipe(rename({suffix: '.min' }))
+    .pipe(minifyCss())
+     .pipe(gulp.dest(config.cssDir))
 });
 
-gulp.task('default', ['scss']);
+gulp.task('default', ['less']);
