9836ff0e748d3b86bdaf1397d7410c4470287957
Jan Rehwaldt <jrehwaldt@cedavis.com>
2015-10-22 14:45:23 +0200

    Working with gulp-less@3.0.1 with cssnano + postcss/csswring *Not* working with gulp-less@3.0.3 or minify-css with any version of gulp-less
    https://github.com/plus3network/gulp-less/issues/166
    https://github.com/plus3network/gulp-less/pull/161
    
    https://github.com/contra/gulp-coffee/pull/42/files

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,43 @@
 var
-    gulp        = require('gulp'), 
+    gulp        = require('gulp'),
     less        = require('gulp-less'),
+    lessStream  = require('less-stream'),
     prefix      = require('gulp-autoprefixer'),
     minifyCss   = require('gulp-minify-css'),
     rename      = require('gulp-rename'),
-    sourcemaps  = require('gulp-sourcemaps');
+    sourcemaps  = require('gulp-sourcemaps')
+    
+    postcss     = require('gulp-postcss'),
+    csswring    = require('csswring'),
+    
+    cssnano     = require('gulp-cssnano');
 
 var config = {
   lessDir: './less/',
   bowerDir: './bower_components/',
   cssDir: './css/'
 };
-gulp.task('less',function(){
+gulp.task('less',function() {
   var paths = [
     config.bowerDir + '/bootstrap/less',
     config.lessDir
   ];
-  return gulp.src( config.lessDir + '**/*.less')
+  return gulp.src(config.lessDir + '**/*.less')
     .pipe(sourcemaps.init())
     .pipe(less({paths: paths}))
+    //.pipe(lessStream({paths: paths}))
     //.pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(config.cssDir))
-    .pipe(rename({suffix: '.min' }))
-    .pipe(minifyCss())
+    .pipe(sourcemaps.write('.', {includeContent: true}))
+    .pipe(gulp.dest(config.cssDir));
+});
+gulp.task('minify',function() {
+  return gulp.src([config.cssDir + '**/*.css', '!' + config.cssDir + '**/*.min.css'])
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(rename({suffix: '.min'}))
+    //.pipe(minifyCss({sourceMap:true}))
+    .pipe(postcss([csswring]))
+    //.pipe(cssnano())
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(config.cssDir));
 });
 
